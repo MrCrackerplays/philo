047d5dd29983496b138889ce00ae1b020fd110b5
pdruart <pdruart@student.codam.nl>
2021-09-22 14:01:36 +0200

    debug progress

--- philo/headers/philosophers.h
+++ philo/headers/philosophers.h
@@ -6,13 +6,19 @@
 /*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/21 11:51:14 by pdruart       #+#    #+#                 */
-/*   Updated: 2021/09/21 14:39:18 by pdruart       ########   odam.nl         */
+/*   Updated: 2021/09/22 13:53:41 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 # define TESTWAARDE 32
+
+typedef struct	s_philosopher
+{
+	void	*a;
+}	t_philosopher;
+
 
 void	test(void);
 
--- philo/src/philosophers.c
+++ philo/src/philosophers.c
@@ -6,11 +6,13 @@
 /*   By: pdruart <marvin@codam.nl>                    +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 12:27:34 by pdruart       #+#    #+#                 */
-/*   Updated: 2021/09/22 11:08:56 by pdruart       ########   odam.nl         */
+/*   Updated: 2021/09/22 13:54:00 by pdruart       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <sys/time.h>
+#include <pthread.h>
 #include <philosophers.h>
 
 void	test(void)
@@ -20,7 +22,9 @@ void	test(void)
 
 int	main(int argc, char **argv)
 {
-
+	struct timeval	time;
+	gettimeofday(&time, NULL);
+	printf("time:%li\n", time.tv_sec);
 	if (argc != 5 && argc != 6)
 		return (0);
 	if (argv)
